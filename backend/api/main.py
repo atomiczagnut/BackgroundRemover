b4e0d3c7718f41efcb80aa7a922f667dd3fd1c4e
atomiczagnut <atomiczagnut@gmail.com>
2026-02-11 06:19:11 -0700

    Update CORS for GitHub Pages

--- backend/api/main.py
+++ backend/api/main.py
@@ -10,8 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "https://atomiczagnut.github.io"
     ],
     allow_credentials=True,
     allow_methods=["POST"],
